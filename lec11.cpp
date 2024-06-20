5119194d40c06006bed7b0249321b1e8160162c7
Ankit Roy <ankitkumar98roy@gmail.com>
2024-06-20 16:40:10 +0530

    Lec11

---
+++ lec11.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i;
+	int ip;
+	while(i<=100){
+		cin>>ip;
+		if(ip==65){
+			cout<<"congratulations no is matched";
+			break;
+		}
+		
+	}
+}
